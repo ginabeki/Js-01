41ffecdab1cf842bbe527a871a0082fac2e12781
ginabeki <ginabeki3@gmail.com>
2022-05-03 14:19:10 +0300

    update test

--- 30DaysofJavascript/Day1/datatypes.js
+++ 30DaysofJavascript/Day1/datatypes.js
@@ -1,37 +1,44 @@
-let firstName = "Gina";
-let isSingle = true;
-let age = 45;
-let country;
-let emptyValue = null;
+// let firstName = "Gina";
+// let isSingle = true;
+// let age = 45;
+// let country;
+// let emptyValue = null;
 
-console.log(typeof "Gina");
-console.log(typeof true);
-console.log(typeof country);
-console.log(typeof null);
-console.log(typeof 45);
+// console.log(typeof "Gina");
+// console.log(typeof true);
+// console.log(typeof country);
+// console.log(typeof null);
+// console.log(typeof 45);
 
-// Declare variables without assigning values
-let country;
-let age;
-let currentStatus;
-let destination;
+// // Declare variables without assigning values
+// let country;
+// let age;
+// let currentStatus;
+// let destination;
 
-// declare variabes with assigned values
-let country = "Malaysia";
-let age = 45;
-let currentStatus = "Single";
-let destination = "Mozambique";
+// // declare variabes with assigned values
+// let country = "Malaysia";
+// let age = 45;
+// let currentStatus = "Single";
+// let destination = "Mozambique";
 
-// Declare variables to store your first name, last name, marital status, country and age in multiple lines
+// // Declare variables to store your first name, last name, marital status, country and age in multiple lines
 
-let firstName = "Gina";
-let maritalStatus = "Married";
-let country = "Germany";
-let age = 45;
+// let firstName = "Gina";
+// let maritalStatus = "Married";
+// let country = "Germany";
+// let age = 45;
 
-// Declare variables to store your first name, last name, marital status, country and age in a single line
+// // Declare variables to store your first name, last name, marital status, country and age in a single line
 
-let firstName,
-  maritalStatus,
-  country,
-  age = " 'GIna', 'Married', 'Germany', 45";
+// let firstName,
+//   maritalStatus,
+//   country,
+//   age = " 'GIna', 'Married', 'Germany', 45";
+
+// Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+
+let myAge = 30;
+let yourAge = 35;
+
+console.log("I am", myAge, "and you are", yourAge);
