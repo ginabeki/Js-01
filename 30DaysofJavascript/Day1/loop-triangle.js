4ae85fe84b4d5c3e6a669e31ed494b66f8b40aee
ginabeki <ginabeki3@gmail.com>
2022-05-04 15:16:19 +0300

    Add question

--- 30DaysofJavascript/Day1/loop-triangle.js
+++ 30DaysofJavascript/Day1/loop-triangle.js
@@ -1,5 +1,18 @@
-// Looping triangle
+/* Looping a triangle
 
+Write a loop that makes seven calls to console.log to output the following triangle:
+
+#
+##
+###
+####
+#####
+######
+#######
+It may be useful to know that you can find the length of a string by writing .length after it.
+*/
+
+// Solution
 for (let line = "#"; line.length < 8; line += "#") {
   console.log(line);
 }
