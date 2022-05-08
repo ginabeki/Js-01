ac2e365b004fb193f217f42835f87821fa14cccd
ginabeki <ginabeki3@gmail.com>
2022-05-08 18:57:43 +0300

    functions

---
+++ 30DaysofJavascript/Day1/functions.js
@@ -0,0 +1,57 @@
+/* function showMessage() {
+  alert("Hello everyone!");
+}
+
+showMessage();
+showMessage();
+*/
+/*
+function logger() {
+  console.log("My name is Gina");
+}
+// invoking, running, calling function
+// call function as many times we want
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice); 
+// function declaration
+function calcAge1(birthYear) {
+  return 2037 - birthYear;
+}
+
+const age1 = calcAge1(1991);
+console.log(age1);
+
+// function expression
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+};
+const age2 = calcAge2(1991);
+console.log(age1, age2);
+*/
+
+// Arrow function
+
+const calcAge3 = (birthYear) => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  //   return retirement;
+  return `${firstName} retires in ${retirement}`;
+};
+console.log(yearsUntilRetirement(1991, "Jonas"));
+console.log(yearsUntilRetirement(1980, "Bob"));
